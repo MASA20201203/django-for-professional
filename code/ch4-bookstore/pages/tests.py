8465867d61217bbdcae52db0886d64000bf92b76
MASA20201203 <koedame032@gmail.com>
2024-12-17 11:20:55 +0900

    update: ch5 p79 url解決のテストを追加

--- code/ch4-bookstore/pages/tests.py
+++ code/ch4-bookstore/pages/tests.py
@@ -1,5 +1,7 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 
 class HomePageTests(SimpleTestCase):
@@ -10,9 +12,6 @@ class HomePageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         self.assertEqual(self.response.status_code, 200)
 
-    def test_homepage_url_name(self):
-        self.assertEqual(self.response.status_code, 200)
-
     def test_homepage_template(self):
         self.assertTemplateUsed(self.response, "home.html")
 
@@ -21,3 +20,7 @@ class HomePageTests(SimpleTestCase):
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
